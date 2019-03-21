d34cd5b99f9dc1eeae9491ccec4f497a12286f45
nour <nour.kilany88@gmail.com>
2019-03-21 20:00:59 +0200

    created members table schema

--- app/Coach.php
+++ app/Coach.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Coach extends Model
 {
     protected $fillable = ["name"];
+    
     public function session()
     {
         return $this->belongsToMany('App\Session', 'session_coach', 'coach_id', 'session_id');
