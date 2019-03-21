7fed5810b686bb79c00c3cb0ebfc336274889aad
nour <nour.kilany88@gmail.com>
2019-03-21 18:40:23 +0200

    Added Package and Session Models with relationship

--- app/Package.php
+++ app/Package.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model
 {
-    //
+    protected $fillable = ["number_of_sessions", "name"];
+
+    public function sessions()
+    {
+        return $this->belongsToMany('App\Session', 'session_package', 'package_id', 'session_id');
+    }
 }
--- app/Session.php
+++ app/Session.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Session extends Model
 {
     protected $fillable = ["name", "number", "price", "start_at", "finish_at"];
+    public function packages()
+    {
+        return $this->belongsToMany('App\Package', 'session_package', 'session_id', 'package_id');
+    }
 }
